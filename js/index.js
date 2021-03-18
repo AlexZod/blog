3b2f055e392804873a501e9d60f0d9d7be8b48e3
AlexZod <61346169+AlexZod@users.noreply.github.com>
2021-03-18 10:54:16 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@
 //     document.querySelector("input").classList.add("feedback__form__clicked")
    
 // })
+// document.ready(function() {
+//     document.getElementsByClassName('.footer__row').addClass("hidden").viewportChecker({
+//     classToAdd: 'visible animated fadeIn',
+//     offset: 100
+//     });
+//     });
